655719151f2c814c58e76dfb94945e3c77c2fbb9
shenghanqin <shenghanqin@163.com>
2024-08-19 03:34:52 +0800

    update home banner. It is very good.

--- src/components/HomeBanner/index.js
+++ src/components/HomeBanner/index.js
@@ -1,14 +1,52 @@
+import { Link } from "react-router-dom";
 import "./styles.css";
 
-const HomeBanner = ({ image }) => {
+const HomeBanner = ({ image, name, summary, officialSite, genres, language, rating }) => {
   return (
     <div
-      className="home-banner__container flex justify-center items-end"
+      className="home-banner__container "
       style={{
         backgroundImage: `linear-gradient(0deg, #141414 0%, rgba(20, 20, 20, 0.00) 100%), url(${image?.original})`,
       }}
     >
-      home-banner
+      <div className="banner-content">
+        <h2 className="banner-title">
+          {name}
+        </h2>
+        <div
+          className="summary-text"
+          dangerouslySetInnerHTML={{
+            __html: summary?.substring(0, 240) + " ...",
+          }}
+        ></div>
+        <div className="banner-info">
+          <Link
+            to={officialSite}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="banner-btn"
+          >
+            <span className="btn-text">Visit Official Site</span>
+            <i className="fas fa-link"></i>
+          </Link>
+          <div className="banner-info-item">
+            <p className="banner-genres">
+              <span>Genre:</span>
+              {genres?.join(", ")}
+            </p>
+            <ul className="banner-info-list">
+              <li className="info-item">
+                <i className="fas fa-language"></i>
+                {language}
+              </li>
+              <li className="info-item-star">
+                <i className="fas fa-star"></i>
+                {rating?.average}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import BetterPicture from "../BetterPicture/index";
-import { Rating } from 'react-simple-star-rating'
 
 import "./styles.css";
 
