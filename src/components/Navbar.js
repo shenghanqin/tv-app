3ea4501675349242466f6b5f5e35115ecd777f28
shenghanqin <shenghanqin@163.com>
2024-08-19 00:58:01 +0800

    update header navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
 import Searchbar from "./Searchbar";
 import { useMediaQuery } from 'react-responsive'
+import { useState } from "react";
 
 const Navbar = () => {
   const isLarge = useMediaQuery({ minWidth: 700 })
+  const [mobileSearchShow, setMobileSearchShow] = useState(false,)
+
+  const toggleMobileSearchShow = () => {
+    setMobileSearchShow(!mobileSearchShow)
+  }
 
   return (
     <div className="navbar">
@@ -21,6 +27,11 @@ const Navbar = () => {
             <li className="links__link">
               <Link to="/about">ABOUT</Link>
             </li>
+            {!isLarge && (
+              <li className="links__link" onClick={toggleMobileSearchShow}>
+                <a className="fas fa-search" />
+              </li>
+            )}
             {isLarge && (
               <li className="links__search">
                 <Searchbar />
@@ -29,7 +40,7 @@ const Navbar = () => {
             
           </ul>
         </nav>
-        {!isLarge && (
+        {!isLarge && mobileSearchShow && (
           <Searchbar />
         )}
       </div>
